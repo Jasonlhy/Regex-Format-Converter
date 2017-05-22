f135176ad6ff2d96c0de37fb881e226833313654
Jason Liu <j-lhy@hotmail.com>
2017-05-23 02:43:11 +0800

    Basic search and replace

--- app.ts
+++ app.ts
@@ -1,1 +1,35 @@
-let a = 2;
+import * as _ from "lodash";
+
+export class PerlToBasic {
+    private characters = {
+        '?': '\\?',
+        '+': '\\+',
+        '{': '\\{',
+        '(': '\\(',
+        ')': '\\)'
+    }
+
+    public convert(perlRegexText: string) {
+        var regexStr = '[' + _.reduce(this.characters, (sum, value, key) => sum + key, '') + ']';
+        var regex = new RegExp(regexStr, 'g');
+        return perlRegexText.replace(regex, (matched) => this.characters[matched]);
+    }
+
+    public revert(basicRegexText: string) {
+        var revertChar: {} = _.transform(this.characters
+            , (sum: {}, value: string, key: string) => sum[value] = key
+            , {});
+
+        let regexStr: string = _.reduce(revertChar
+            , (sum: string, value: string, key: string) =>
+                (!sum) ? (_.escapeRegExp(key)) : (sum + "|" + _.escapeRegExp(key))
+            , '');
+
+        var regObj = new RegExp(regexStr, "g");
+        return basicRegexText.replace(regObj, (matched) => revertChar[matched]);
+    }
+}
+
+let p = new PerlToBasic();
+console.log("convert: " + p.convert("aaa+?"));
+console.log("revert: " + p.revert("aaa\\+\\?"));
